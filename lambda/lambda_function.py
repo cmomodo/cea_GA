effc6a29c7e6d0a0fc0da6476e8b728721470cf5
cmomodo <ceesay.ml@outlook.com>
2024-08-22 12:59:01 +0100

    added comments

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     return {
+        #returns this when its true
         'statusCode': 200,
         'body': json.dumps('Hello from our CI/CD workflows vscode, Ceesay is back!')
     }
