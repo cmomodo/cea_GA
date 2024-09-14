9d199d3c5b07deab3cff07195e3026d9b2246283
cmomodo <ceesay.ml@outlook.com>
2024-09-14 23:12:05 +0100

    just for showcase

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,10 @@
-import json 
+import json
 
 def lambda_handler(event, context):
     return {
         #returns this when its true
         'statusCode': 200,
-        'body': json.dumps('Ceesay is back!')
+        'body': json.dumps('Ceesay showing deemoney is back!')
     }
 
 #deploy function test
